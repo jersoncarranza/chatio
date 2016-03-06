5ae695964d18c079d77067df789f2b53999def9f
jerson@hotmail.com <jerson@hotmail.com>
2016-03-05 23:39:21 -0500

    puerto

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-/*
-var http  = require('http');
-var expressServer = require('./ExpressServer0.js');
-
-
-var app = new expressServer();
-var server = http.createServer(app.expressServer);
-var port = normalizePort(process.env.PORT || 5000)
-server.listen(port);
-console.log('escuchando el puerto', port);
-*/
-
-//app.engine('html', swig.renderFile);
-/*app.set('view options', {
-  layout: false
-});*/
 var express = require('express');
 var app = express();
 
 var server = require('http').createServer(app);
 var	io= require('socket.io').listen(server); 
-var nicknames =[];
+var users = {};
+//var nicknames =[];
 
 app.set("view engine", "jade");
 app.use(express.static("./public")); 
@@ -33,35 +18,75 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('newuser', function (data, callback) {
 		console.log("el nuevo usuario", data);
-		if(nicknames.indexOf(data) != -1){
+		//if(nicknames.indexOf(data) != -1){
+		if(data in users){
 			callback(false);
-		}else{
+		}
+		else{
 			callback(true);
 			socket.nickname  = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
+			//nicknames.push(socket.nickname);
 			//io.sockets.emit('usernames', nicknames);
 			updateNicknames();
 		}
 	});
 
-	socket.on('send message', function (data) {
+	socket.on('send message', function (data, callback) {
 		console.log("el message es:", data);
-		io.sockets.emit('new message',{ msg: data , nick: socket.nickname });
+		var msg = data.trim();
+		if(msg.substr(0,3) === '/w ')
+		{
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if (ind !== -1) {
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1);
+				if(name in users){
+					users[name].emit('whisper',{ msg: data , nick: socket.nickname });
+					console.log("whisper");
+				}else{
+					console.log("Error el mensaje no se envio");
+					callback('Error: enter a valid user');
+				}
+			}else{
+				callback.log('Error: Please enter a message for your whisper');
+			}
+		}else{
+		io.sockets.emit('new message',{ msg: msg , nick: socket.nickname });
 		//socket.broadcast.emit('new message', data);
+		}
+
 	});
 
 	socket.on('disconnect', function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		delete users[socket.nickname];
+		//nicknames.splice(nicknames.indexOf(socket.nickname),1);
 		updateNicknames();
 	});
 
 	function updateNicknames () {
-		io.sockets.emit('usernames', nicknames);
+		//io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	};
 
 });
 
-server.listen(3000);
-//app.listen(3000);
-console.log('hola');
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+var port = normalizePort(process.env.PORT || 5000);
+server.listen(port);
+console.log('Puerto:', port);
