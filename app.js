44a435184ba0990a0c122386a3e6c4e94c46e77d
jerson@hotmail.com <jerson@hotmail.com>
2016-03-07 23:07:02 -0500

    Deploy heroku

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var http  = require('http');
 var expressServer = require('./ExpressServer.js');
 var socketIO= require('./app/socketio.js');
 
-var app = new expressServer();
-var server = http.createServer(app.expressServer);
-var Io = new socketIO({server:server});
 //var	io= require('socket.io').listen(server); 
 function normalizePort(val) {
   var port = parseInt(val, 10);
@@ -18,7 +15,10 @@ function normalizePort(val) {
   return false;
 }
 
+var app = new expressServer();
+var server = http.createServer(app.expressServer);
+var Io = new socketIO({server:server});
 
 var port = normalizePort(process.env.PORT || 5000);
 server.listen(port);
-console.log('Escuchando Puerto:', port);
+console.log('Escuchando Puerto N0:', port);
